fdc68893d93a692000a4a79b3574abca6fa6501a
ZENOTME <st810918843@gmail.com>
2025-03-07 16:10:54 +0800

    support partition writer

--- crates/iceberg/src/writer/function_writer/mod.rs
+++ crates/iceberg/src/writer/function_writer/mod.rs
@@ -15,15 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-//! Conversion between Iceberg and Arrow schema
+//! This module contains the functional writer.
 
-mod schema;
-pub use schema::*;
-mod reader;
-pub(crate) mod record_batch_projector;
-pub(crate) mod record_batch_transformer;
-mod value;
-pub use reader::*;
-pub use value::*;
-mod record_batch_partition_spliter;
-pub(crate) use record_batch_partition_spliter::*;
+pub mod fanout_partition_writer;
+pub mod precompute_partition_writer;
