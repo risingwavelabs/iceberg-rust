6f2fd2eac1c66e059b379823b6b1afb8ba7053aa
xxhZs <1060434431@qq.com>
2025-03-13 15:12:12 +0800

    add pos

--- crates/integrations/datafusion/src/physical_plan/file_task_scan.rs
+++ crates/integrations/datafusion/src/physical_plan/file_task_scan.rs
@@ -21,7 +21,7 @@ use std::sync::Arc;
 use std::vec;
 
 use async_stream::try_stream;
-use datafusion::arrow::array::{Int64Array, RecordBatch};
+use datafusion::arrow::array::{Int64Array, RecordBatch, StringArray};
 use datafusion::arrow::datatypes::{Field, Schema, SchemaRef as ArrowSchemaRef};
 use datafusion::error::Result as DFResult;
 use datafusion::execution::{SendableRecordBatchStream, TaskContext};
@@ -133,6 +133,7 @@ async fn get_batch_stream(
 ) -> DFResult<Pin<Box<dyn Stream<Item = DFResult<RecordBatch>> + Send>>> {
     let stream = try_stream! {
         for task in file_scan_tasks {
+            let file_path = task.data_file_path.clone();
             let data_file_content = task.data_file_content;
             let sequence_number = task.sequence_number;
             let task_stream = futures::stream::iter(vec![Ok(task)]).boxed();
@@ -141,12 +142,15 @@ async fn get_batch_stream(
                 .read(task_stream)
                 .await
                 .map_err(to_datafusion_error)?;
-
+            let mut index_start = 0;
             while let Some(batch) = batch_stream.next().await {
                 let batch = batch.map_err(to_datafusion_error)?;
                 let batch = match data_file_content {
                     iceberg::spec::DataContentType::Data => {
-                        add_seq_num_into_batch(batch, sequence_number)?
+                        let batch = add_seq_num_into_batch(batch, sequence_number)?;
+                        let batch = add_file_path_pos_into_batch(batch, &file_path, index_start)?;
+                        index_start += batch.num_rows() as i64;
+                        batch
                     }
                     iceberg::spec::DataContentType::PositionDeletes => {
                         batch
@@ -164,17 +168,43 @@ async fn get_batch_stream(
 
 fn add_seq_num_into_batch(batch: RecordBatch, seq_num: i64) -> DFResult<RecordBatch> {
     let schema = batch.schema();
-    let new_field = Arc::new(Field::new(
+    let seq_num_field = Arc::new(Field::new(
         "seq_num",
         datafusion::arrow::datatypes::DataType::Int64,
         false,
     ));
     let mut new_fields = schema.fields().to_vec();
-    new_fields.push(new_field);
+    new_fields.push(seq_num_field);
     let new_schema = Arc::new(Schema::new(new_fields));
 
     let mut columns = batch.columns().to_vec();
     columns.push(Arc::new(Int64Array::from(vec![seq_num; batch.num_rows()])));
+    RecordBatch::try_new(new_schema, columns)
+        .map_err(|e| datafusion::error::DataFusionError::ArrowError(e, None))
+}
+
+fn add_file_path_pos_into_batch(batch: RecordBatch, file_path: &str, index_start: i64) -> DFResult<RecordBatch> {
+    let schema = batch.schema();
+    let file_path_field = Arc::new(Field::new(
+        "file_path",
+        datafusion::arrow::datatypes::DataType::Utf8,
+        false,
+    ));
+    let pos_field = Arc::new(Field::new(
+        "pos",
+        datafusion::arrow::datatypes::DataType::Utf8,
+        false,
+    ));
+    let mut new_fields = schema.fields().to_vec();
+    new_fields.push(file_path_field);
+    new_fields.push(pos_field);
+    let new_schema = Arc::new(Schema::new(new_fields));
+
+    let mut columns = batch.columns().to_vec();
+    columns.push(Arc::new(StringArray::from(vec![file_path; batch.num_rows()])));
+    columns.push(Arc::new(Int64Array::from_iter(
+        (index_start..(index_start + batch.num_rows() as i64)).collect::<Vec<i64>>(),
+    )));
     RecordBatch::try_new(new_schema, columns)
         .map_err(|e| datafusion::error::DataFusionError::ArrowError(e, None))
 }
