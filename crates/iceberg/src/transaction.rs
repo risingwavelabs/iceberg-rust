97538627cdd09de3ee3f22ea7dcefff327a9a16b
xxchan <xxchan22f@gmail.com>
2025-03-25 17:24:52 +0800

    Revert "cherry pick fast append api (#25)"
    This reverts commit fe854025248f0818c9a82a07b81f8367baea58b5.

--- crates/iceberg/src/transaction.rs
+++ crates/iceberg/src/transaction.rs
@@ -111,8 +111,7 @@ impl<'a> Transaction<'a> {
         Ok(self)
     }
 
-    /// Generate a unique snapshot id.
-    pub fn generate_unique_snapshot_id(&self) -> i64 {
+    fn generate_unique_snapshot_id(&self) -> i64 {
         let generate_random_id = || -> i64 {
             let (lhs, rhs) = Uuid::new_v4().as_u64_pair();
             let snapshot_id = (lhs ^ rhs) as i64;
@@ -137,26 +136,10 @@ impl<'a> Transaction<'a> {
     /// Creates a fast append action.
     pub fn fast_append(
         self,
-        snapshot_id: Option<i64>,
         commit_uuid: Option<Uuid>,
         key_metadata: Vec<u8>,
     ) -> Result<FastAppendAction<'a>> {
-        let snapshot_id = if let Some(snapshot_id) = snapshot_id {
-            if self
-                .table
-                .metadata()
-                .snapshots()
-                .any(|s| s.snapshot_id() == snapshot_id)
-            {
-                return Err(Error::new(
-                    ErrorKind::DataInvalid,
-                    format!("Snapshot id {} already exists", snapshot_id),
-                ));
-            }
-            snapshot_id
-        } else {
-            self.generate_unique_snapshot_id()
-        };
+        let snapshot_id = self.generate_unique_snapshot_id();
         FastAppendAction::new(
             self,
             snapshot_id,
@@ -911,7 +894,7 @@ mod tests {
     async fn test_fast_append_action() {
         let table = make_v2_minimal_table();
         let tx = Transaction::new(&table);
-        let mut action = tx.fast_append(None, None, vec![]).unwrap();
+        let mut action = tx.fast_append(None, vec![]).unwrap();
 
         // check add data file with incompatible partition value
         let data_file = DataFileBuilder::default()
@@ -1018,7 +1001,7 @@ mod tests {
             format!("{}/3.parquet", &fixture.table_location),
         ];
 
-        let fast_append_action = tx.fast_append(None, None, vec![]).unwrap();
+        let fast_append_action = tx.fast_append(None, vec![]).unwrap();
 
         // Attempt to add the existing Parquet files with fast append.
         let new_tx = fast_append_action
--- crates/integration_tests/tests/shared_tests/append_partition_data_file_test.rs
+++ crates/integration_tests/tests/shared_tests/append_partition_data_file_test.rs
@@ -119,7 +119,7 @@ async fn test_append_partition_data_file() {
 
     // commit result
     let tx = Transaction::new(&table);
-    let mut append_action = tx.fast_append(None, None, vec![]).unwrap();
+    let mut append_action = tx.fast_append(None, vec![]).unwrap();
     append_action
         .add_data_files(data_file_valid.clone())
         .unwrap();
@@ -178,7 +178,7 @@ async fn test_schema_incompatible_partition_type(
     let data_file_invalid = data_file_writer_invalid.close().await.unwrap();
 
     let tx = Transaction::new(&table);
-    let mut append_action = tx.fast_append(None, None, vec![]).unwrap();
+    let mut append_action = tx.fast_append(None, vec![]).unwrap();
     if append_action
         .add_data_files(data_file_invalid.clone())
         .is_ok()
@@ -217,7 +217,7 @@ async fn test_schema_incompatible_partition_fields(
     let data_file_invalid = data_file_writer_invalid.close().await.unwrap();
 
     let tx = Transaction::new(&table);
-    let mut append_action = tx.fast_append(None, None, vec![]).unwrap();
+    let mut append_action = tx.fast_append(None, vec![]).unwrap();
     if append_action
         .add_data_files(data_file_invalid.clone())
         .is_ok()
