9f37306d8ba3e98dfb6d416a611e137cd50a8685
congyi <15605187270@163.com>
2025-03-26 22:09:50 +0800

    add comment

--- crates/iceberg/src/transaction/mod.rs
+++ crates/iceberg/src/transaction/mod.rs
@@ -131,6 +131,7 @@ impl<'a> Transaction<'a> {
         Ok(self)
     }
 
+    /// Generate a new `snapshot_id`.
     pub fn generate_unique_snapshot_id(&self) -> i64 {
         let generate_random_id = || -> i64 {
             let (lhs, rhs) = Uuid::new_v4().as_u64_pair();
