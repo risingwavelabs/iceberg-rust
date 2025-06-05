af79eae236ce689a5cb0c551a700f2017ae4e7d7
Jannik Steinmann <jannik.steinmann@datadoghq.com>
2025-06-05 16:23:24 +0200

    feat(storage-azdls): Add Azure Datalake Storage support (#1368)
    - Closes #1360.
    
    This PR adds an integration for the Azure Datalake storage service. At
    it's core, it adds parsing logic for configuration properties. The
    finished config struct is simply passed down to OpenDAL. In addition it
    adds logic to parse fully qualified file URIs, and matches it against
    expected (previously configured) values.
    
    It also creates a new `Storage::Azdls` enum variant based on OpenDAL's
    existing `Scheme::Azdls` enum variant. It then fits the parsing logic
    into the existing framework to build the storage integration from an
    `io::FileIOBuilder`.
    
    Other Iceberg ADLS integrations ([pyiceberg +
    Java](https://github.com/apache/iceberg-go/pull/313/files#r2021460617))
    also support the `wasb://` and `wasbs://` schemes.
    WASB refers to a client-side implementation of hierarchical namespaces
    on top of Blob Storage. ADLS(v2) on the other hand is a service offered
    by Azure, also built on top of Blob Storage.
    IIUC we can accept both schemes because objects written to Blob Storage
    via `wasb://` will also be accessible via `adfs://` (which operates on
    the same Blob Storage).
    Even though the URIs slightly differ in format when they refer to the
    same object, we can largely reuse existing logic.
    ```diff
    -wasb[s]://<containername>@<accountname>.blob.core.windows.net/<path>
    +adfs[s]://<filesystemname>@<accountname>.dfs.core.windows.net/<path>
    ```
    
    I added minor unit tests to validate the configuration property parsing
    logic.
    
    I decided **not** to add integration tests because
    1. ADLS is not S3-compatible which means that we can't reuse our Minio
    setup
    2. the Azure-specific alternative to local testing - Azurite - doesn't
    support ADLS
    
    I have yet to test it in a functioning environment.
    
    ---------
    
    Signed-off-by: Jannik Steinmann <jannik.steinmann@datadoghq.com>

--- crates/iceberg/src/io/storage.rs
+++ crates/iceberg/src/io/storage.rs
@@ -20,12 +20,16 @@ use std::sync::Arc;
 use opendal::layers::{RetryLayer, TimeoutLayer};
 #[cfg(feature = "storage-azblob")]
 use opendal::services::AzblobConfig;
+#[cfg(feature = "storage-azdls")]
+use opendal::services::AzdlsConfig;
 #[cfg(feature = "storage-gcs")]
 use opendal::services::GcsConfig;
 #[cfg(feature = "storage-s3")]
 use opendal::services::S3Config;
 use opendal::{Operator, Scheme};
 
+#[cfg(feature = "storage-azdls")]
+use super::AzureStorageScheme;
 use super::FileIOBuilder;
 use crate::{Error, ErrorKind};
 
@@ -36,22 +40,30 @@ pub(crate) enum Storage {
     Memory(Operator),
     #[cfg(feature = "storage-fs")]
     LocalFs,
+    /// Expects paths of the form `s3[a]://<bucket>/<path>`.
     #[cfg(feature = "storage-s3")]
     S3 {
         /// s3 storage could have `s3://` and `s3a://`.
         /// Storing the scheme string here to return the correct path.
-        scheme_str: String,
-        /// uses the same client for one FileIO Storage.
-        ///
-        /// TODO: allow users to configure this client.
-        client: reqwest::Client,
+        configured_scheme: String,
         config: Arc<S3Config>,
     },
     #[cfg(feature = "storage-gcs")]
     Gcs { config: Arc<GcsConfig> },
-
     #[cfg(feature = "storage-azblob")]
     Azblob { config: Arc<AzblobConfig> },
+    #[cfg(feature = "storage-oss")]
+    Oss { config: Arc<OssConfig> },
+    /// Expects paths of the form
+    /// `abfs[s]://<filesystem>@<account>.dfs.<endpoint-suffix>/<path>` or
+    /// `wasb[s]://<container>@<account>.blob.<endpoint-suffix>/<path>`.
+    #[cfg(feature = "storage-azdls")]
+    Azdls {
+        /// Because Azdls accepts multiple possible schemes, we store the full
+        /// passed scheme here to later validate schemes passed via paths.
+        configured_scheme: AzureStorageScheme,
+        config: Arc<AzdlsConfig>,
+    },
 }
 
 impl Storage {
@@ -67,8 +79,7 @@ impl Storage {
             Scheme::Fs => Ok(Self::LocalFs),
             #[cfg(feature = "storage-s3")]
             Scheme::S3 => Ok(Self::S3 {
-                scheme_str,
-                client: reqwest::Client::new(),
+                configured_scheme: scheme_str,
                 config: super::s3_config_parse(props)?.into(),
             }),
             #[cfg(feature = "storage-gcs")]
@@ -79,6 +90,14 @@ impl Storage {
             Scheme::Azblob => Ok(Self::Azblob {
                 config: super::azblob_config_parse(props)?.into(),
             }),
+            #[cfg(feature = "storage-azdls")]
+            Scheme::Azdls => {
+                let scheme = scheme_str.parse::<AzureStorageScheme>()?;
+                Ok(Self::Azdls {
+                    config: super::azdls_config_parse(props)?.into(),
+                    configured_scheme: scheme,
+                })
+            }
             // Update doc on [`FileIO`] when adding new schemes.
             _ => Err(Error::new(
                 ErrorKind::FeatureUnsupported,
@@ -125,15 +144,14 @@ impl Storage {
             }
             #[cfg(feature = "storage-s3")]
             Storage::S3 {
-                scheme_str,
-                client,
+                configured_scheme,
                 config,
             } => {
-                let op = super::s3_config_build(client, config, path)?;
+                let op = super::s3_config_build(config, path)?;
                 let op_info = op.info();
 
                 // Check prefix of s3 path.
-                let prefix = format!("{}://{}/", scheme_str, op_info.name());
+                let prefix = format!("{}://{}/", configured_scheme, op_info.name());
                 if path.starts_with(&prefix) {
                     Ok((op, &path[prefix.len()..]))
                 } else {
@@ -169,11 +187,18 @@ impl Storage {
                     ))
                 }
             }
+            #[cfg(feature = "storage-azdls")]
+            Storage::Azdls {
+                configured_scheme,
+                config,
+            } => super::azdls_create_operator(path, config, configured_scheme),
             #[cfg(all(
                 not(feature = "storage-s3"),
                 not(feature = "storage-fs"),
                 not(feature = "storage-gcs"),
-                not(feature = "storage-azblob")
+                not(feature = "storage-azblob"),
+                not(feature = "storage-oss"),
+                // not(feature = "storage-azdls"),
             ))]
             _ => Err(Error::new(
                 ErrorKind::FeatureUnsupported,
@@ -195,6 +220,8 @@ impl Storage {
             "file" | "" => Ok(Scheme::Fs),
             "s3" | "s3a" => Ok(Scheme::S3),
             "gs" | "gcs" => Ok(Scheme::Gcs),
+            "oss" => Ok(Scheme::Oss),
+            "abfss" | "abfs" | "wasbs" | "wasb" => Ok(Scheme::Azdls),
             s => Ok(s.parse::<Scheme>()?),
         }
     }
